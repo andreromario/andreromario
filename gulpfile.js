a2c2f153bf9b2ab80ef2961098c64af45e685c67
André Romário <andreromario@live.com>
2017-02-22 16:38:08 -0300

    Transforming website to Pug(Old Jade) and Stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,104 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
+var reload = browserSync.reload;
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
-
-// Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
-].join('');
-
-// Compile LESS files from /less into /css
-gulp.task('less', function() {
-    return gulp.src('less/new-age.less')
+var stylus = require('gulp-stylus');
+var autoPrefixer = require('gulp-autoprefixer');
+//if node version is lower than v.0.1.2
+require('es6-promise').polyfill();
+var cssComb = require('gulp-csscomb');
+var cmq = require('gulp-merge-media-queries');
+var cleanCss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var pug = require('gulp-pug');
+var minifyHtml = require('gulp-minify-html');
+var imageMin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+gulp.task('stylus',function(){
+    gulp.src(['src/stylus/*.styl'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(stylus({
+            'include css': true
+        }))
         .pipe(sourcemaps.init())
-        .pipe(less())
-        .pipe(header(banner, { pkg: pkg }))
+        .pipe(autoPrefixer())
+        .pipe(cssComb())
+        .pipe(cmq({log:true}))
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('css'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(cleanCss())
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+        .pipe(reload({stream:true}))
 });
+gulp.task('js',function(){
+    var src = 'src/js/';
 
-// Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/new-age.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
+    var jquery = src + 'jquery.min.js';
+    var bootstrap = src + 'bootstrap.min.js';
+    var easing = src + 'jquery.easing.min.js';
+    var fancybox = src + 'jquery.fancybox.min.js';
+    var scripts = src + 'scripts.js';
+
+    gulp.src([jquery,bootstrap,easing,fancybox,scripts])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
         }))
-});
-
-// Minify JS
-gulp.task('minify-js', function() {
-    return gulp.src('js/new-age.js')
-        .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
         .pipe(gulp.dest('js'))
-        .pipe(browserSync.reload({
-            stream: true
+        .pipe(rename({
+            suffix: '.min'
         }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('js'))
+        .pipe(reload())
 });
-
-// Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
-
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
-
-    gulp.src(['node_modules/simple-line-icons/*/*'])
-        .pipe(gulp.dest('vendor/simple-line-icons'))
-
-
-    gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('vendor/font-awesome'))
-})
-
-// Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
-
-// Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task('pug',function(){
+    gulp.src(['src/*.pug'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(pug())
+        .pipe(minifyHtml())
+        .pipe(gulp.dest('./'))
+});
+gulp.task('img',function(){
+    gulp.src(['src/img/**/*'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(cache(imageMin()))
+        .pipe(gulp.dest('img'))
+        .pipe(reload())
+});
+gulp.task('default',function(){
     browserSync.init({
-        server: {
-            baseDir: ''
-        },
-    })
-})
-
-// Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
-    gulp.watch('less/*.less', ['less']);
-    gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('js/*.js', ['minify-js']);
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
+        server: "./"
+    });
+    gulp.watch('src/js/**/*.js',['js']);
+    gulp.watch('src/stylus/**/*.styl',['stylus']);
+    gulp.watch('src/**/*.pug',['pug']);
+    gulp.watch('src/img/**/*',['img']);
 });
